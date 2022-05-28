739cd9c22ed452ea17dcd43a13bab3489f902062
vjo001 <89363371+vjo001@users.noreply.github.com>
2022-05-27 20:50:11 -0400

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,29 @@
+colors = ['purple', 'red', 'orange', 'blue', 'pink']
+print(colors);
+
+colors.append('yellow')
+print('Updated colors list: ', colors)
+
+foods = ['pizza', 'fries', 'sandwich', 'ice cream']
+print(foods);
+
+foods.append('cheese')
+print('Updated foods list: ', foods)
+
+colors.append(foods)
+print('Updated list ', colors)
+
+letters = ['v', 'i', 'n', 'c', 'e']
+print(letters)
+
+letters.remove('e')
+print('Updated letters list: ', letters)
+
+numbers = ['1', '2', '3', '4', '5']
+print(numbers);
+
+numbers.append('6')
+print('Updated numbers list: ', numbers)
+
+numbers.remove('1')
+print('Updated number list: ', numbers)
